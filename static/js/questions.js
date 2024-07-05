21602616a33732999ba00183c709dcc3eecc060d
Ruthik <ksairuthik2004@gmail.com>
2024-07-05 19:36:48 +0530

    Added

---
+++ static/js/questions.js
@@ -0,0 +1,78 @@
+// Questions as an array of Objects
+var questions = [
+    {
+      title: 'Number of primitive data types in Java are?',
+      choices: ['6', '7', '8', '9'],
+      answer: '8'
+    },
+    {
+      title: 'What is the size of float and double in java?',
+      choices: ['32 and 64', '32 and 32', '64 and 64', '64 and 32'],
+      answer: '32 and 64'
+    },
+    {
+      title: 'Automatic type conversion is possible in which of the possible cases?',
+      choices: ['Byte to Int', 'Int to Long', 'Long to Int', 'Short to Int'],
+      answer: 'Int to Long'
+    },
+    {
+      title: 'Select the valid statement.',
+      choices: ['char[] ch = new char(5)', 'char[] ch = new char[5]', 'char[] ch = new char()', 'char[] ch = new char[]'],
+      answer: 'char[] ch = new char[5]'
+    },
+    {
+      title: 'When an array is passed to a method, what does the method receive?',
+      choices: ['The reference of the array', 'A copy of the array', 'Length of the array', 'Copy of first element'],
+      answer: 'The reference of the array'
+    },
+    {
+      title: 'Select the valid statement to declare and initialize an array.',
+      choices: ['int[] A={}', 'int []A={1,2,3}', 'int[] A=(1,2,3)', 'int[][] A={1,2,3}'],
+      answer: 'int []A={1,2,3}'
+    },
+    {
+      title: 'Arrays in java are-',
+      choices: ['Object references', 'objects', 'Primitive Data type', 'None of these'],
+      answer: 'objects'
+    },
+    {
+      title: 'What is used to create panoramic photos in computer vision?',
+      choices: ['Image rotation', 'Image resizing', 'Image stitching', 'Image mirroring'],
+      answer: 'Image stitching'
+    },
+    {
+      title: ' When is the object created with new keyword?',
+      choices: ['At run time', 'At compile time', 'Depends on the code', 'None of these'],
+      answer: 'At run time'
+    },
+    {
+      title: 'Identify the corrected definition of a package.',
+      choices: ['A package is a collection of editing tools', 'A package is a collection of classes', ' A package is a collection of classes and interfaces', 'A package is a collection of  interfaces'],
+      answer: 'A package is a collection of classes and interfaces'
+    },
+    {
+      title: 'What is the primary objective of image segmentation in computer vision?',
+      choices: ['To remove color information from images', 'To classify images into categories', 'To locate objects within an image and separate them from the background', 'To increase image resolution'],
+      answer: 'To locate objects within an image and separate them from the background'
+    },
+    {
+      title: 'Identify the correct restriction on static methods. 1)They must access only static data. 2)They can only call other static methods. 3)They cannot refer to this or super.',
+      choices: ['I and II', 'II and III', 'Only III', 'I,II and III'],
+      answer: 'I,II and III'
+    },
+    {
+      title: 'Identify the keyword among the following that makes a variable belong to a class,rather than being defined for each instance of the class',
+      choices: ['final', 'static', 'volatile', 'abstract'],
+      answer: 'static'
+    },
+    {
+      title: 'In which of the following is toString() method defined?',
+      choices: ['java.lang.Object', 'java.lang.String', 'java.lang.util', 'None of these'],
+      answer: 'java.lang.Object'
+    },
+    {
+      title: ' When is the finalize() method called?',
+      choices: ['Before garbage Collection', 'Before an object goes out of scope', 'Both 1 and 2', 'None of these'],
+      answer: 'Before garbage Collection'
+    }
+  ];
